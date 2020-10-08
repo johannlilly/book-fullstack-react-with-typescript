632127975d32e9460eefd8904b609e4800e02c89
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-08 14:29:13 -0400

    how to manually add children to our interface

--- exercise-files/01-first-app/class-components/src/Column.tsx
+++ exercise-files/01-first-app/class-components/src/Column.tsx
@@ -1,6 +1,18 @@
 import React from "react"
 import { ColumnContainer, ColumnTitle } from "./styles"
 
+/**
+ * Here is the React.PropsWithChildren type definition:
+
+type React.PropsWithChildren<P> = P & {
+  children?: React.ReactNode;
+}
+
+ * The letter P in angle brackets is a generic type.
+ * It serves as a placeholder for an actual type that we can pass there. 
+ * It doesn’t necessarily have to be P, but the convention is to use capital Latin letters.
+ */
+
 interface ColumnProps {
   text: string
 }
