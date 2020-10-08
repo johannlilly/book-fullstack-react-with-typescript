be6307af5632539e04f74a79ba960c9485d23872
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-08 15:19:14 -0400

    understanding the AddNewItemProp interface

--- exercise-files/01-first-app/step2/src/AddNewItem.tsx
+++ exercise-files/01-first-app/step2/src/AddNewItem.tsx
@@ -3,9 +3,9 @@ import { NewItemForm } from "./NewItemForm"
 import { AddItemButton } from "./styles"
 
 interface AddNewItemProps {
-  onAdd(text: string): void
-  toggleButtonText: string
-  dark?: boolean
+  onAdd(text: string): void // callback function that will be called when we click the Create item button.
+  toggleButtonText: string // text we’ll render when this component is a button
+  dark?: boolean // flag that we’ll pass to the styled component
 }
 
 export const AddNewItem = (props: AddNewItemProps) => {
