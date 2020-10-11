33ce52a3212ce6d862be079a97278b75994d285d
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-11 11:13:47 -0400

    enable add new item to column

--- exercise-files/01-first-app/step2/src/Column.tsx
+++ exercise-files/01-first-app/step2/src/Column.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { AddNewItem } from "./AddNewItem"
 import { ColumnContainer, ColumnTitle } from "./styles"
 
 interface ColumnProps {
@@ -13,6 +14,11 @@ export const Column = ({
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
       {children}
+      <AddNewItem
+        toggleButtonText="+ Add another task"
+        onAdd={console.log}
+        dark
+      />
     </ColumnContainer>
   )
 }
