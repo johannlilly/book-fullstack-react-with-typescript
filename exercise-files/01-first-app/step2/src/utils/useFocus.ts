0b0c191b8629040c7437b8216630b2ab7a5639e7
Johann Lilly 李光 يوهان ليل Джон Лилли <johannlilly@users.noreply.github.com>
2020-10-11 11:16:40 -0400

    useRef hook to get access to the rendered input element

--- exercise-files/01-first-app/step2/src/utils/useFocus.ts
+++ exercise-files/01-first-app/step2/src/utils/useFocus.ts
@@ -1,5 +1,12 @@
 import { useRef, useEffect } from "react"
 
+/**
+ * Here we use the useRef hook to get access to the rendered input element.
+ * TypeScript can’t automatically know what will be the element type.
+ * So we provide the actual type to it.
+ * In our case, we work with input so it’s HTMLInputElement.
+ */
+
 export const useFocus = () => {
   const ref = useRef<HTMLInputElement>(null)
 
